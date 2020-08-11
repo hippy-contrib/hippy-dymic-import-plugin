9adddd1de4a36d15db2692d8a8bf4441eb4a6c25
melodyren <melodyren@tencent.com>
2020-08-11 14:36:35 +0800

    feat: readme说明

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# hippy 支持动态import插件
+# hippy 支持动态 import 插件
+
+## 介绍：
 
 对于⼤的 Hippy 应用来讲，将所有的代码都放在⼀个文件中显然是不够有效的，特别是当你的某些代码块是在某些特殊的时候才会被使用到。
 
@@ -6,4 +8,8 @@
 
 webpack 支持将你的代码库分割成 chunks(语块)，当代码运行到需要它们的时候再进行加载。通过脚本懒加载，使得初始下载的代码更⼩
 
-由于hippy 不支持 动态创建script ，需要改写 __webpack_require__.e， 通过修改 compilation.mainTemplate.hooks.requireEnsure 来改写 __webpack_require__.e;
+由于 hippy 不支持 动态创建 script ，需要改写 **webpack_require**.e， 通过修改 compilation.mainTemplate.hooks.requireEnsure 来改写 **webpack_require**.e;
+
+## 方案贡献者
+
+stevezhuo、melodyren
--- index.js
+++ index.js
@@ -6,96 +6,96 @@ class HippyJsonpTemplatePlugin {
 			const tapsOfRequireEnsuer = compilation.mainTemplate.hooks.requireEnsure.taps;
 			// console.log("=====HippyJsonpTemplatePlugin======");
 			for (let i = 0; i < tapsOfRequireEnsuer.length; i++) {
-			  if (tapsOfRequireEnsuer[i].name === "JsonpMainTemplatePlugin load") {
-				tapsOfRequireEnsuer.splice(i, 1);
-				compilation.mainTemplate.hooks.requireEnsure.tap(
-				   "JsonpMainTemplatePlugin load",
-				   (source, chunk, hash) => {
-						const chunkLoadTimeout = compilation.mainTemplate.outputOptions.chunkLoadTimeout;
-						return Template.asString([
-							source,
-							"",
-							"// JSONP chunk loading for javascript",
-							"",
-							"var installedChunkData = installedChunks[chunkId];",
-
-							'if(installedChunkData !== 0) { // 0 means "already installed".',
-							Template.indent([
+				if (tapsOfRequireEnsuer[i].name === "JsonpMainTemplatePlugin load") {
+					tapsOfRequireEnsuer.splice(i, 1);
+					compilation.mainTemplate.hooks.requireEnsure.tap(
+						"JsonpMainTemplatePlugin load",
+						(source, chunk, hash) => {
+							const chunkLoadTimeout = compilation.mainTemplate.outputOptions.chunkLoadTimeout;
+							return Template.asString([
+								source,
 								"",
-								'// a Promise means "currently loading".',
-								"if(installedChunkData) {",
-								Template.indent(["promises.push(installedChunkData[2]);"]),
-								"} else {",
+								"// JSONP chunk loading for javascript",
+								"",
+								"var installedChunkData = installedChunks[chunkId];",
+
+								'if(installedChunkData !== 0) { // 0 means "already installed".',
 								Template.indent([
-									"// setup Promise in chunk cache",
-									"var promise = new Promise(function(resolve, reject) {",
-									Template.indent([
-										"installedChunkData = installedChunks[chunkId] = [resolve, reject];"
-									]),
-									"});",
-									"promises.push(installedChunkData[2] = promise);",
 									"",
-									"// start chunk loading",
-									// "function runCode(code) { (0, eval)('this').eval(code); }",
-									"function runCode(code) { new Function(code)() }",
-									"var script = {}",
-									"var onScriptComplete;",
-									`script.timeout = ${chunkLoadTimeout / 1000};`,
-									"script.src = jsonpScriptSrc(chunkId);",
-									"console.log('请求地址:',script.src)",
-									"fetch(script.src).then(res => {",
-										Template.indent([	
+									'// a Promise means "currently loading".',
+									"if(installedChunkData) {",
+									Template.indent(["promises.push(installedChunkData[2]);"]),
+									"} else {",
+									Template.indent([
+										"// setup Promise in chunk cache",
+										"var promise = new Promise(function(resolve, reject) {",
+										Template.indent([
+											"installedChunkData = installedChunks[chunkId] = [resolve, reject];"
+										]),
+										"});",
+										"promises.push(installedChunkData[2] = promise);",
+										"",
+										"// start chunk loading",
+										// "function runCode(code) { (0, eval)('this').eval(code); }",
+										"function runCode(code) { new Function(code)() }",
+										"var script = {}",
+										"var onScriptComplete;",
+										`script.timeout = ${chunkLoadTimeout / 1000};`,
+										"script.src = jsonpScriptSrc(chunkId);",
+										"console.log('请求地址:',script.src)",
+										"fetch(script.src).then(res => {",
+										Template.indent([
 											// "console.info(res,'返回成功后数据')",
 											"runCode(res.body);",
 											"script.onload();"
 										]),
-									"}).catch(err => {",
+										"}).catch(err => {",
 										// "console.error(err,'返回失败后数据')",
-										Template.indent([	
+										Template.indent([
 											"script.onerror({ type: 'error', src: err.userInfo && err.userInfo.NSErrorFailingURLStringKey || err})",
 										]),
-									"})",
-									"// create error before stack unwound to get useful stacktrace later",
-									"var error = new Error();",
-									"onScriptComplete = function (event) {",
-									Template.indent([
-										"// avoid mem leaks in IE.",
-										"script.onerror = script.onload = null;",
-										"clearTimeout(timeout);",
-										"var chunk = installedChunks[chunkId];",
-										"if(chunk !== 0) {",
+										"})",
+										"// create error before stack unwound to get useful stacktrace later",
+										"var error = new Error();",
+										"onScriptComplete = function (event) {",
 										Template.indent([
-											"if(chunk) {",
+											"// avoid mem leaks in IE.",
+											"script.onerror = script.onload = null;",
+											"clearTimeout(timeout);",
+											"var chunk = installedChunks[chunkId];",
+											"if(chunk !== 0) {",
 											Template.indent([
-												"var errorType = event && (event.type === 'load' ? 'missing' : event.type);",
-												"var realSrc = event.src",
-												"error.message = 'Loading chunk ' + chunkId + ' failed.\\n(' + errorType + ': ' + realSrc + ')';",
-												"error.name = 'ChunkLoadError';",
-												"error.type = errorType;",
-												"error.request = realSrc;",
-												"chunk[1](error);"
+												"if(chunk) {",
+												Template.indent([
+													"var errorType = event && (event.type === 'load' ? 'missing' : event.type);",
+													"var realSrc = event.src",
+													"error.message = 'Loading chunk ' + chunkId + ' failed.\\n(' + errorType + ': ' + realSrc + ')';",
+													"error.name = 'ChunkLoadError';",
+													"error.type = errorType;",
+													"error.request = realSrc;",
+													"chunk[1](error);"
+												]),
+												"}",
+												"installedChunks[chunkId] = undefined;"
 											]),
-											"}",
-											"installedChunks[chunkId] = undefined;"
+											"}"
 										]),
-										"}"
-									]),
-									"};",
-									"var timeout = setTimeout(function(){",
+										"};",
+										"var timeout = setTimeout(function(){",
 										Template.indent([
 											"onScriptComplete({ type: 'timeout', target: script });"
 										]),
-									`}, ${chunkLoadTimeout});`,
-									"script.onerror = script.onload = onScriptComplete;"
+										`}, ${chunkLoadTimeout});`,
+										"script.onerror = script.onload = onScriptComplete;"
+									]),
+									"}"
 								]),
 								"}"
-							]),
-							"}"
-						]);
-					}
-				);
-				break;
-			  }
+							]);
+						}
+					);
+					break;
+				}
 			}
 			callback(null, compilation);
 		});
